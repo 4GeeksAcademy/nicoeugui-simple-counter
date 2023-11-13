5ebc66130674ff8b4b6322bb7deb15ef6e2a54f2
Nicolas Eugui <74990164+NicoEugui@users.noreply.github.com>
2023-11-13 23:43:04 +0000

    stop, reset, resume function

--- src/js/component/secondsCounter.jsx
+++ src/js/component/secondsCounter.jsx
@@ -1,17 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CardNumber from './cardNumber'
 import Title from './title';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faClock } from '@fortawesome/free-regular-svg-icons';
-import CounterControl from './counterControl';
 
 const SecondsCounter = (props) => {
 
-
     return (
         <div>
-            
-            <Title/>
+
+            <Title />
 
             <div className='d-flex position-absolute top-50 start-50 translate-middle bg-dark w-auto p-3 rounded-1'>
 
@@ -27,9 +25,14 @@ const SecondsCounter = (props) => {
                     return <CardNumber digito={value[1]} key={value[0]} />
                 })}
 
-                <CounterControl/>
+            <div className='d-flex m-5'>
+                <button type="button" className="btn btn-success"
+                    onClick={props.handleResume}>Resume</button>
+                <button type="button" className="btn btn-danger" onClick={props.handlePause}>Pause</button>
+                <button type="button" className="btn btn-warning" onClick={props.handleStop} >Reset</button>
             </div>
 
+            </div>
         </div>
     )
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 //import react into the bundle
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
@@ -7,31 +7,66 @@ import "../styles/index.css";
 
 //import your own components
 import SecondsCounter from "./component/secondsCounter.jsx";
+import { faL } from "@fortawesome/free-solid-svg-icons";
+
+//const [pause, setPause] = useState(false);
+
+let pause = false;
+let stop = false;
+
+let counter = 0;
 
 let objetc = {};
 let listDigits = [];
 
-let counter = 0;
-setInterval(()=>{
-    const six = Math.floor((counter/100000) % 10);
-    const five = Math.floor((counter/10000) % 10);
-    const four = Math.floor((counter/1000) % 10);
-    const three = Math.floor((counter/100) % 10);
-    const two = Math.floor((counter/10) % 10);
-    const one = Math.floor((counter/1) % 10);
-    counter++;
+const handlePause = () => {
+    pause = true;
+}
 
-    objetc.six = six;
-    objetc.five = five;
-    objetc.four = four;
-    objetc.three = three;
-    objetc.two = two;
-    objetc.one = one;
+const handleResume = () => {
+    pause = false;
+}
 
-    listDigits = [...Object.entries(objetc)];
+const handleStop = () => {
+    console.log(stop)
+    stop = true;
+}
 
-    ReactDOM.render(<SecondsCounter listDigits = {listDigits} />, 
-        document.querySelector("#app"));
+setInterval(() => {
+
+    if (!pause && !stop) {
+        const six = Math.floor((counter / 100000) % 10);
+        const five = Math.floor((counter / 10000) % 10);
+        const four = Math.floor((counter / 1000) % 10);
+        const three = Math.floor((counter / 100) % 10);
+        const two = Math.floor((counter / 10) % 10);
+        const one = Math.floor((counter / 1) % 10);
+        counter++;
+
+        objetc.six = six;
+        objetc.five = five;
+        objetc.four = four;
+        objetc.three = three;
+        objetc.two = two;
+        objetc.one = one;
+
+        listDigits = [...Object.entries(objetc)];
+
+        ReactDOM.render(<SecondsCounter
+            listDigits={listDigits}
+            pause={pause}
+            handlePause={handlePause}
+            handleResume={handleResume}
+            handleStop={handleStop}
+        />,
+            document.querySelector("#app"));
+    } else if (stop) {
+
+        counter = 0;
+        stop = false;
+        pause = false;
+
+    } 
 
 }, 1000);
 
